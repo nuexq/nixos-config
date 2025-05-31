8cbfa794a1c36a31e3484cb1b1e7693fbd9ee76e
Frost-Phoenix <67cyril6767@gmail.com>
2025-05-31 21:12:32 +0200

    delete aseprite

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,7 +1,6 @@
 { ... }:
 {
   imports = [
-    ./aseprite/aseprite.nix           # pixel art editor
     ./audacious.nix                   # music player
     ./bat.nix                         # better cat command
     ./browser.nix                     # firefox based browser
