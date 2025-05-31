27b094afcf0c70ad84c115c13e2062b929ed96ba
Frost-Phoenix <67cyril6767@gmail.com>
2025-05-31 21:02:23 +0200

    ghostty

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -13,75 +13,37 @@ in
   xdg.configFile."ghostty/config".text = ''
     # Font
     font-family = "Maple Mono"
-    font-size = ${if (host == "laptop") then "16" else "17"}
-    font-feature = calt
-    font-feature = ss03
+    font-size = 9
+    font-thicken = true
 
-    bold-is-bright = false
-    selection-invert-fg-bg = true
+    adjust-underline-position = 40%
+    adjust-underline-thickness = -60%
 
-    # Theme
-    theme = "gruvbox"
-    background-opacity = 0.66
-
-    cursor-style = bar
+    cursor-style = block
     cursor-style-blink = false
-    adjust-cursor-thickness = 1
-
-    resize-overlay = never
-    copy-on-select = false
-    confirm-close-surface = false
     mouse-hide-while-typing = true
 
-    window-theme = ghostty
-    # window-padding-x = 4
-    # window-padding-y = 6
-    window-padding-balance = true
-    window-padding-color = background
-    window-inherit-working-directory = true
-    window-inherit-font-size = true
+    shell-integration = zsh
+    shell-integration-features = no-cursor
+
+    window-width = 120
+    window-height = 45
+    window-padding-x = 4
+
+    confirm-close-surface = false
+    clipboard-read = allow
+    clipboard-paste-protection = false
+    unfocused-split-opacity = 1.0
+
+    gtk-tabs-location = hidde
     window-decoration = false
 
-    gtk-titlebar = false
-    gtk-single-instance = false
-    gtk-tabs-location = bottom
-    gtk-wide-tabs = false
+    background-opacity = 1
+    background-blur-radius = 20
 
-    auto-update = off
-    term = ghostty
-    clipboard-paste-protection = false
-
-    keybind = shift+end=unbind
-    keybind = shift+home=unbind
-    keybind = ctrl+shift+left=unbind
-    keybind = ctrl+shift+right=unbind
-    keybind = shift+enter=text:\n
-  '';
-  xdg.configFile."ghostty/themes/gruvbox".text = ''
-    background = #1d2021
-    foreground = #fbf1c7
-
-    palette = 0=#32302f
-    palette = 1=#cc241d
-    palette = 2=#98971a
-    palette = 3=#d79921
-    palette = 4=#458588
-    palette = 5=#b16286
-    palette = 6=#689d6a
-    palette = 7=#ebdbb2
-
-    palette = 8=#928374
-    palette = 9=#fb4934
-    palette = 10=#b8bb26
-    palette = 11=#fabd2f
-    palette = 12=#83a598
-    palette = 13=#d3869b
-    palette = 14=#8ec07c
-    palette = 15=#fbf1c7
-
-    cursor-color = #D5C4A1
-
-    selection-foreground = #282828
-    selection-background = #98971A
+    window-padding-x = 10
+    window-padding-y = 8
+    window-padding-color = extend
+    theme = catppuccin-mocha
   '';
 }
