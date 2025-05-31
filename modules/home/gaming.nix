513c7fbd81dccaf95f5502da2f5bbfbc081c4b1d
Frost-Phoenix <67cyril6767@gmail.com>
2025-05-31 21:04:02 +0200

    update gaming.nix

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,32 +1,9 @@
 { pkgs, inputs, ... }:
 {
   home.packages = with pkgs; [
-    ## Utils
-    # gamemode
-    # gamescope
-    # winetricks
-    # inputs.nix-gaming.packages.${pkgs.system}.wine-ge
+    gamemode
 
     ## Minecraft
     # prismlauncher
-
-    ## Cli games
-    _2048-in-terminal
-    vitetris
-    nethack
-
-    ## Celeste
-    celeste-classic
-    celeste-classic-pm
-
-    ## Doom
-    # gzdoom
-    crispy-doom
-
-    ## Emulation
-    sameboy
-    snes9x
-    # cemu
-    # dolphin-emu
   ];
 }
