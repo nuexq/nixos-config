32389d7023116369be80cce85ceb0b2577129d9e
nuexq <hi.yassir.op@gmail.com>
2025-06-01 14:35:36 +0200

    fzf theme

--- modules/home/fzf.nix
+++ modules/home/fzf.nix
@@ -16,13 +16,14 @@
 
     ## Theme
     defaultOptions = [
-      "--color=fg:#cad3f5,fg+:#b7bdf8,bg:#24273a,bg+:#363a4f"
-      "--color=hl:#8aadf4,hl+:#91d7e3,info:#a6da95,marker:#ed8796"
-      "--color=prompt:#f5a97f,spinner:#8bd5ca,pointer:#ed8796,header:#c6a0f6"
-      "--color=border:#363a4f,label:#b7bdf8,query:#cad3f5"
       "--border='double' --border-label='' --preview-window='border-sharp' --prompt='> '"
       "--marker='>' --pointer='>' --separator='─' --scrollbar='│'"
       "--info='right'"
+      "--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8"
+      "--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC"
+      "--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8"
+      "--color=selected-bg:#45475A"
+      "--color=border:#313244,label:#CDD6F4"
     ];
 
   };
