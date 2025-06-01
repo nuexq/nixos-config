14959e2cd6e0309afafd28d2912e0c66a355de48
nuexq <hi.yassir.op@gmail.com>
2025-06-01 22:54:04 +0200

    adding zsh alliases

--- modules/home/zsh/zsh_alias.nix
+++ modules/home/zsh/zsh_alias.nix
@@ -3,22 +3,21 @@
   programs.zsh = {
     shellAliases = {
       # Utils
-      c = "clear";
       cd = "z";
       cat = "bat";
       diff = "delta --diff-so-fancy --side-by-side";
       less = "bat";
       pdf = "tdf";
       open = "xdg-open";
-      man = "BAT_THEME="default" batman";
+      man = "BAT_THEME='default' batman";
 
-      c="clear" # clear terminal
-      l="eza -lh  --icons=auto" # long list
-      ls="eza -1   --icons=auto" # short list
-      ll="eza -lha --icons=auto --sort=name --group-directories-first" # long list all
-      ld="eza -lhD --icons=auto" # long list dirs
-      pl="$aurhelper -Qs" # list installed package
-      nv="nvim" # nvim
+      c = "clear"; # clear terminal
+      l = "eza -lh  -icons=auto"; # long list
+      ls = "eza -1   --icons=auto"; # short list
+      ll = "eza -lha --icons=auto --sort=name --group-directories-first"; # long list all
+      ld = "eza -lhD --icons=auto"; # long list dirs
+      pl = "$aurhelper -Qs"; # list installed package
+      nv = "nvim"; # nvim
 
       # Nixos
       cdnix = "cd ~/nixos-config && codium ~/nixos-config";
@@ -32,11 +31,11 @@
       nix-test = "nh os test";
 
       # Handy change dir shortcuts
-      ..="cd .."
-      ...="cd ../.."
-      .3="cd ../../.."
-      .4="cd ../../../.."
-      .5="cd ../../../../.."
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      ".3" = "cd ../../..";
+      ".4" = "cd ../../../..";
+      ".5" = "cd ../../../../..";
     };
   };
 }
