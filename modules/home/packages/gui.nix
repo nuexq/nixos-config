34dfda15fb53b726c50b4d5f33537375a3f42870
nuexq <hi.yassir.op@gmail.com>
2025-06-01 16:13:32 +0200

    update gui.nix

--- modules/home/packages/gui.nix
+++ modules/home/packages/gui.nix
@@ -2,24 +2,11 @@
 {
   home.packages = with pkgs; [
     ## Multimedia
-    gimp
     obs-studio
     pavucontrol
-    pitivi # video editing
     vlc
 
-    ## Office
-    libreoffice
-    gnome-calculator
-
     ## Utility
-    dconf-editor
-    gnome-disk-utility
-    mission-center # GUI resources monitor
     zenity
-
-    ## Level editor
-    ldtk
-    tiled
   ];
 }
