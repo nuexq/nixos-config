7fcc74dac56826e0981cbdba5f95c2a1f2797215
nuexq <hi.yassir.op@gmail.com>
2025-06-02 11:06:40 +0200

    change gtk font

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -4,18 +4,14 @@
   home.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     nerd-fonts.fira-code
-    nerd-fonts.caskaydia-cove
-    nerd-fonts.symbols-only
-    twemoji-color-font
     noto-fonts-emoji
-    fantasque-sans-mono
-    maple-mono.truetype-autohint
+    maple-mono.NF-unhinted
   ];
 
   gtk = {
     enable = true;
     font = {
-      name = "Maple Mono NF";
+      name = "JetBrain Nerd Font";
       size = 12;
     };
     theme = {
