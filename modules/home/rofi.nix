e4c8b3b2e1be6d5db3fdad670863595886d850fa
nuexq <hi.yassir.op@gmail.com>
2025-06-02 17:29:48 +0200

    change rofi style

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -4,24 +4,103 @@
 
   xdg.configFile."rofi/theme.rasi".text = ''
     * {
-      bg-col: #1D2021;
-      bg-col-light: #282828;
-      border-col: #A89984;
-      selected-col: #3C3836;
-      green: #98971A;
-      fg-col: #FBF1C7;
-      fg-col2: #EBDBB2;
-      grey: #BDAE93;
-      highlight: @green;
+      font: "JetBrainsMono Nerd Font";
+
+      bg0: #1e1e2e;
+      bg1: #313244;
+      fg0: #cdd6f4;
+
+      accent-color: #45475a;
+      urgent-color: #f9e2af;
+
+      background-color: transparent;
+      text-color: @fg0;
+
+      margin: 0;
+      padding: 0;
+      spacing: 0;
+    }
+
+    window {
+      location: center;
+      width: 480;
+      background-color: @bg0;
+    }
+
+    inputbar {
+      spacing: 8px;
+      padding: 8px;
+      background-color: @bg1;
+    }
+
+    prompt, entry, element-icon, element-text {
+      vertical-align: 0.5;
+    }
+
+    prompt {
+      text-color: @accent-color;
+    }
+
+    textbox {
+      padding: 8px;
+      background-color: @bg1;
+    }
+
+    listview {
+      padding: 4px 0;
+      lines: 8;
+      columns: 1;
+      fixed-height: false;
+    }
+
+    element {
+      padding: 8px;
+      spacing: 8px;
+    }
+
+    element normal normal {
+      text-color: @fg0;
+    }
+
+    element normal urgent {
+      text-color: @urgent-color;
+    }
+
+    element normal active {
+      text-color: @accent-color;
+    }
+
+    element alternate active {
+      text-color: @accent-color;
+    }
+
+    element selected {
+      text-color: @bg0;
+    }
+
+    element selected normal, element selected active {
+      background-color: @accent-color;
+    }
+
+    element selected urgent {
+      background-color: @urgent-color;
+    }
+
+    element-icon {
+      size: 0.8em;
+    }
+
+    element-text {
+      text-color: inherit;
     }
   '';
 
   xdg.configFile."rofi/config.rasi".text = ''
-    configuration{
+    configuration {
       modi: "run,drun,window";
-      lines: 5;
+      lines: 8;
       cycle: false;
-      font: "Maple Mono NF Bold 16";
+      font: "FiraCode Nerd Font Medium 12";
       show-icons: true;
       icon-theme: "Papirus-dark";
       terminal: "ghostty";
@@ -32,99 +111,20 @@
       display-drun: " Apps ";
       display-run: " Run ";
       display-window: " Window ";
-      /* display-Network: " Network"; */
       sidebar-mode: true;
       sorting-method: "fzf";
     }
 
     @theme "theme"
 
-    element-text, element-icon , mode-switcher {
+    element-text, element-icon, mode-switcher {
       background-color: inherit;
-      text-color:       inherit;
-    }
-
-    window {
-      height: 530px;
-      width: 400px;
-      border: 2px;
-      border-color: @border-col;
-      background-color: @bg-col;
-    }
-
-    mainbox {
-      background-color: @bg-col;
-    }
-
-    inputbar {
-      children: [prompt,entry];
-      background-color: @bg-col-light;
-      border-radius: 5px;
-      padding: 0px;
-    }
-
-    prompt {
-      background-color: @green;
-      padding: 4px;
-      text-color: @bg-col-light;
-      border-radius: 3px;
-      margin: 10px 0px 10px 10px;
-    }
-
-    textbox-prompt-colon {
-      expand: false;
-      str: ":";
-    }
-
-    entry {
-      padding: 6px;
-      margin: 10px 10px 10px 5px;
-      text-color: @fg-col;
-      background-color: @bg-col;
-      border-radius: 3px;
-    }
-
-    listview {
-      border: 0px 0px 0px;
-      padding: 6px 0px 0px;
-      margin: 10px 0px 0px 6px;
-      columns: 1;
-      background-color: @bg-col;
-      cycle: true;
-    }
-
-    element {
-      padding: 8px;
-      margin: 0px 10px 4px 4px;
-      background-color: @bg-col;
-      text-color: @fg-col;
-    }
-
-    element-icon {
-      size: 28px;
-    }
-
-    element selected {
-      background-color:  @selected-col ;
-      text-color: @fg-col2  ;
-      border-radius: 3px;
+      text-color: inherit;
     }
 
     mode-switcher {
       spacing: 0;
     }
-
-    button {
-      padding: 10px;
-      background-color: @bg-col-light;
-      text-color: @grey;
-      vertical-align: 0.5;
-      horizontal-align: 0.5;
-    }
-
-    button selected {
-      background-color: @bg-col;
-      text-color: @green;
-    }
   '';
 }
+
