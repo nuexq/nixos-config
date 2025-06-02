803d58bf402fc4df70d30bcac366a6ab4f887726
nuexq <hi.yassir.op@gmail.com>
2025-06-02 13:45:23 +0200

    hypridle

--- modules/home/hyprland/hypridle.nix
+++ modules/home/hyprland/hypridle.nix
@@ -6,24 +6,25 @@
   programs.hypridle = {
     enable = true;
     settings = {
+      general = {
+        lock_cmd = "hyprlock";
+        before_sleep_cmd = "hyprlock";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
 
-    general = {
-      lock_cmd = "hyprlock";
-      before_sleep_cmd = "hyprlock";
-      after_sleep_cmd = "hyprctl dispatch dpms on"
+      listeners = [
+        {
+          timeout = 600;
+          on-timeout = "hyprlock";
+          on-resume = "";
+        }
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend";
+          on-resume = "";
+        }
+      ];
     };
-
-    listeners = [
-      {
-        timeout = 600;
-        on-timeout = "hyprlock";
-        on-resume = "";
-      }
-      {
-        timeout = 1800;
-        on-timeout = "systemctl suspend";
-        on-resume = "";
-      }
-    ];
-  }
+  };
 }
+
