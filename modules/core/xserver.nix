af4c9d647bc66e7e66e30f4ea012147a587aec57
nuexq <hi.yassir.op@gmail.com>
2025-06-02 17:45:05 +0200

    sddm minecraft theme (minesddm)

--- modules/core/xserver.nix
+++ modules/core/xserver.nix
@@ -1,14 +1,42 @@
 { username, pkgs, ... }:
 
+let
+  minesddm = pkgs.stdenv.mkDerivation rec {
+    pname = "minesddm";
+    version = "1.0";
+
+    dontBuild = true;
+
+    src = pkgs.fetchFromGitHub {
+      owner = "Davi-S";
+      repo = "sddm-theme-minesddm";
+      rev = "main";
+      sha256 = "1n0frzm66hy3hiz029xnxji3qb4w5x7ali7k6n71nfgbwwjmcin9";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes
+      cp -aR $src/minesddm $out/share/sddm/themes/minesddm
+    '';
+  };
+in
 {
+  environment.systemPackages = with pkgs; [
+    minesddm
+  ];
+
   services = {
     xserver = {
       enable = true;
       xkb.layout = "us,ara";
 
-      displayManager.sddm.enable = true;
-
-      displayManager.autoLogin.enable = false;
+      displayManager = {
+        autoLogin.enable = false;
+        sddm = {
+          enable = true;
+          theme = "minesddm";
+        };
+      };
     };
 
     libinput.enable = true;
