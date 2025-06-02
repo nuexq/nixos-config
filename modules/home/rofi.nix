6bd7370de7089b77217b387f1ca8226d7945a512
nuexq <hi.yassir.op@gmail.com>
2025-06-02 11:13:36 +0200

    everything use maple mono NF

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -21,7 +21,7 @@
       modi: "run,drun,window";
       lines: 5;
       cycle: false;
-      font: "Maple Mono Bold 16";
+      font: "Maple Mono NF Bold 16";
       show-icons: true;
       icon-theme: "Papirus-dark";
       terminal: "ghostty";
