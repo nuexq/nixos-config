199f2ef0b1b0278c1c49327dbefea4a875aef444
nuexq <hi.yassir.op@gmail.com>
2025-06-03 18:24:36 +0200

    remove fzf theme

--- modules/home/fzf.nix
+++ modules/home/fzf.nix
@@ -13,18 +13,5 @@
     changeDirWidgetOptions = [
       "--preview 'eza --tree --color=always {} | head -200'"
     ];
-
-    ## Theme
-    defaultOptions = [
-      "--border='double' --border-label='' --preview-window='border-sharp' --prompt='> '"
-      "--marker='>' --pointer='>' --separator='─' --scrollbar='│'"
-      "--info='right'"
-      "--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8"
-      "--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC"
-      "--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8"
-      "--color=selected-bg:#45475A"
-      "--color=border:#313244,label:#CDD6F4"
-    ];
-
   };
 }
