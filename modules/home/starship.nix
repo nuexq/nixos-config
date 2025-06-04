87bae924e26021b0deaad3c0996cb5f86aea9f7c
nuexq <hi.yassir.op@gmail.com>
2025-06-04 12:00:43 +0100

    update starship

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -6,8 +6,11 @@
 
     settings = {
       add_newline = true;
-
-      character = { use_symbol_for_status = false; };
+      character = {
+        disabled = false;
+        success_symbol = "[❯](green)";
+        error_symbol = "[❯](red)";
+      };
 
       directory = {
         style = "purple";
@@ -19,27 +22,7 @@
 
       git_branch = { symbol = " "; };
 
-      git_commit = {
-        disabled = false;
-        commit_hash_length = 4;
-        tag_disabled = false;
-      };
-
-      git_state = { disabled = true; };
-
       git_status = {
-        # disabled = false;
-        # ignore_submodules = false;
-        # ahead = "[ ($count) ](blue)";
-        # behind = "[ ($count) ](yellow)";
-        # deleted = "[ ($count) ](red)";
-        # diverged = "[ﰬ ($count) ](red)";
-        # modified = "[ ($count) ](yellow)";
-        # renamed = "[ ($count) ](purple)";
-        # staged = "[ ($count) ](cyan)";
-        # stashed = "[ ($count) ](blue)";
-        # format = "[ $all_status$ahead_behind]()";
-
         conflicted_count = { enabled = true; };
         untracked_count = { enabled = true; };
         modified_count = { enabled = true; };
@@ -49,33 +32,12 @@
         stashed_count = { enabled = true; };
       };
 
-      kubernetes = {
-        symbol = "⎈ ";
-        disabled = true;
-      };
-
       line_break = { disabled = false; };
 
-      nix_shell = { disabled = true; };
+      rust = { symbol = ""; };
+      nix_shell = { symbol = ""; };
 
-      memory_usage = { disabled = true; };
-
-      java = { symbol = " "; };
-
-      julia = { symbol = " "; };
-
-      nodejs = { symbol = ""; };
-
-      package = { symbol = ""; };
-
-      python = { };
-
-      ruby = { };
-
-      rust = { symbol = " "; };
-
-      username = { disabled = false; };
-
+      cmd_duration = { disabled = true; };
     };
   };
 }
