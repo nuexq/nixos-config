7805cab5a2ea50d3f091369e5ef6b880d34b8925
nuexq <hi.yassir.op@gmail.com>
2025-06-04 13:41:02 +0100

    update starship

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -6,11 +6,17 @@
 
     settings = {
       add_newline = true;
+
+      format = ''
+         $directory$git_branch$git_status
+        $character'';
+
       character = {
         disabled = false;
         success_symbol = "[❯](green)";
         error_symbol = "[❯](red)";
       };
+
       directory = {
         style = "blue";
         format = "[$path]($style) ";
@@ -18,25 +24,28 @@
         truncation_symbol = ".../";
         truncate_to_repo = false;
       };
+
       git_branch = {
         symbol = " ";
         style = "bold green";
         format = "on [$symbol$branch]($style) ";
       };
+
       git_status = {
         format = "[$all_status]()";
-        ahead = "[$count](green)";
-        behind = "[$count](yellow)";
-        staged = "[$count](cyan)";
-        conflicted = "[$count](red)";
-        modified = "[$count](yellow)";
-        deleted = "[$count](red)";
-        untracked = "[$count](blue)";
-        renamed = "[$count](purple)";
-        stashed = "[$count](blue)";
+        ahead = "[ ($count)](green)"; # arrow-up
+        behind = "[ ($count)](yellow)"; # arrow-down
+        staged = "[ ($count)](cyan)"; # checkmark
+        conflicted = "[ ($count)](red)"; # conflict icon
+        modified = "[ ($count)](yellow)"; # pencil
+        deleted = "[ ($count)](red)"; # trash or x icon
+        untracked = "[ ($count)](blue)"; # question mark icon
+        renamed = "[➜ ($count)](purple)"; # arrow
+        stashed = "[ ($count)](blue)"; # package icon
       };
+
       line_break = { disabled = false; };
-      cmd_duration = { disabled = true; };
     };
+
   };
 }
