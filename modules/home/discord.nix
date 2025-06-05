add873334a2938f683c838811de004158d36fca8
nuexq <hi.yassir.op@gmail.com>
2025-06-05 20:20:35 +0100

    fmt

--- modules/home/discord.nix
+++ modules/home/discord.nix
@@ -1,8 +1,3 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    (discord.override {
-      withVencord = true;
-    })
-  ];
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ discord.override { withVencord = true; } ];
 }
