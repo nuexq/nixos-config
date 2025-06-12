8d19db919afb2e776f97cb0005b53782a4639f69
nuexq <hi.yassir.op@gmail.com>
2025-06-12 12:33:54 +0100

    adding back nix-gaming + lowlatency enable

--- flake.nix
+++ flake.nix
@@ -37,6 +37,8 @@
       };
     };
 
+    inputs.nix-gaming.url = "github:fufexan/nix-gaming";
+
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
