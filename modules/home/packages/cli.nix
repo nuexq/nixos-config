1438dc5a72cb8a33a67874959ff7f45ca1c47ffb
nuexq <hi.yassir.op@gmail.com>
2025-06-13 11:46:00 +0100

    adding comments to packages/cli.nix

--- modules/home/packages/cli.nix
+++ modules/home/packages/cli.nix
@@ -2,41 +2,40 @@
 {
   home.packages = with pkgs; [
     aoc-cli                           # Advent of Code command-line tool
-    dysk                              # disk information
+    dysk                              # Disk information
     eza                               # ls replacement
     fd                                # find replacement
-    ffmpeg
-    imv                               # image viewer
+    ffmpeg                            # Audio/video converter and processor
+    imv                               # Image viewer
     jq                                # JSON processor
-    libnotify
-    lowfi
-    man-pages                         # extra man pages
-    mpv                               # video player
-    nitch                             # systhem fetch util
-    openssl
-    onefetch                          # fetch utility for git repo
-    pamixer                           # pulseaudio command line mixer
-    playerctl                         # controller for media players
-    poweralertd
-    programmer-calculator
+    libnotify                         # Notifications library
+    lowfi                             # Terminal low-fi mode
+    man-pages                         # Extra man pages
+    mpv                               # Video player
+    nitch                             # System fetch utility
+    onefetch                          # Git repo summary in terminal
+    pamixer                           # PulseAudio command-line mixer
+    playerctl                         # Controller for media players
+    poweralertd                       # Alerts when running on battery
+    programmer-calculator             # Programmer’s calculator (CLI)
     ripgrep                           # grep replacement
-    shfmt                             # bash formatter
-    swappy                            # snapshot editing tool
-    tdf                               # cli pdf viewer
-    treefmt                           # project formatter
-    tldr
-    unzip
-    wl-clipboard                      # clipboard utils for wayland (wl-copy, wl-paste)
-    wget
-    woomer
-    yt-dlp-light
-    xdg-utils
-    xxd
+    shfmt                             # Bash formatter
+    swappy                            # Snapshot editing tool
+    tdf                               # CLI PDF viewer
+    treefmt                           # Project-wide formatter
+    tldr                              # Simplified man pages
+    unzip                             # Extract .zip archives
+    wl-clipboard                      # Clipboard utils for Wayland (wl-copy, wl-paste)
+    wget                              # Command-line file downloader
+    woomer                            # GitHub release fetcher for Wayland software
+    yt-dlp-light                      # Lightweight YouTube downloader
+    xdg-utils                         # Desktop integration utils (open files/URLs)
 
-    cbonsai                           # terminal screensaver
-    cmatrix
-    pipes                             # terminal screensaver
-    sl
-    tty-clock                         # cli clock
+    cbonsai                           # Terminal screensaver (ASCII bonsai)
+    cmatrix                           # Matrix effect in terminal
+    pipes                             # Terminal pipes animation
+    sl                                # Steam locomotive animation (for mistyped 'ls')
+    tty-clock                         # CLI clock
   ];
 }
+
