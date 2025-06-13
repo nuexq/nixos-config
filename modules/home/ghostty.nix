88a063bc152e530b349b6796ad02d5494cbc7a4e
nuexq <hi.yassir.op@gmail.com>
2025-06-13 20:45:07 +0100

    update ghostty

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -1,48 +1,39 @@
-{
-  inputs,
-  pkgs,
-  host,
-  ...
-}:
-let
-  ghostty = inputs.ghostty.packages.${pkgs.system}.default;
-in
-{
-  home.packages = [ ghostty ];
+{ config, ... }: {
+  programs.ghostty = {
+    enable = true;
 
-  xdg.configFile."ghostty/config".text = ''
-    # Font
-    font-family = "Maple Mono NF CN"
-    font-size = 9
-    font-thicken = true
+    settings = {
 
-    adjust-underline-position = 40%
-    adjust-underline-thickness = -60%
+      theme = "catppuccin-${config.catppuccin.flavor}";
+      font-family = "Maple Mono NF CN";
+      font-size = 9;
+      font-thicken = true;
 
-    cursor-style = block
-    cursor-style-blink = false
-    mouse-hide-while-typing = true
+      #adjust-underline-position = "40%";
+      #adjust-underline-thickness = "-60%";
 
-    shell-integration = zsh
-    shell-integration-features = no-cursor
+      cursor-style = "block";
+      cursor-style-blink = false;
+      mouse-hide-while-typing = true;
 
-    window-width = 120
-    window-height = 45
-    window-padding-x = 10
-    window-padding-y = 8,0
+      shell-integration = "zsh";
+      shell-integration-features = "no-cursor";
 
-    confirm-close-surface = false
-    clipboard-read = allow
-    clipboard-paste-protection = false
-    unfocused-split-opacity = 1.0
+      window-width = 120;
+      window-height = 45;
+      window-padding-x = 10;
+      window-padding-y = 8;
+      window-padding-color = "background";
 
-    gtk-tabs-location = hidden
-    window-decoration = false
+      confirm-close-surface = false;
+      clipboard-read = "allow";
+      clipboard-paste-protection = false;
 
-    background-opacity = 1
-    background-blur-radius = 20
+      gtk-tabs-location = "hidden";
+      window-decoration = false;
 
-    window-padding-color = extend
-    theme = catppuccin-mocha
-  '';
+      #background-opacity = 1;
+      #background-blur-radius = 20;
+    };
+  };
 }
