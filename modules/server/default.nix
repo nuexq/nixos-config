a74698ea7b8cf8cdebcc76866af3d66d67c8ec60
nuexq <hi.yassir.op@gmail.com>
2025-06-22 21:48:01 +0100

    samba

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -2,6 +2,6 @@
   imports = [
     ./minecraft.nix # minecraft server
     ./jellyfin.nix  # jellyfin
-    ./nfs.nix       # nfs server
+    ./samba.nix
   ];
 }
---
+++ modules/server/samba.nix
@@ -0,0 +1,18 @@
+{ username, ... }:
+
+{
+  services.samba = {
+    enable = true;
+    shares = {
+      shared = {
+        path = "/home/${username}/shared";
+        browseable = true;
+        writable = true;
+        guestOk = false;
+        validUsers = [ "nuexq" ];
+      };
+    };
+    security = { serverSigning = "auto"; };
+  };
+}
+
