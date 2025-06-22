3ee391787649dd9c1d27b8a4ac7454544d6a5efa
nuexq <hi.yassir.op@gmail.com>
2025-06-22 22:16:44 +0100

    media

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./minecraft.nix # minecraft server
     ./jellyfin.nix  # jellyfin
+    ./media.nix
   ];
 }
---
+++ modules/server/media.nix
@@ -0,0 +1,63 @@
+{ config, ... }:
+
+let
+  # Download client config path
+  qbittorrentConfig = "/var/lib/qbittorrent/config/qbittorrent.conf";
+in
+{
+  services.radarr = {
+    enable = true;
+    dataDir = "/srv/media/radarr";
+    port = 7878;
+    user = "nuexq";
+    group = "users";
+  };
+
+  services.sonarr = {
+    enable = true;
+    dataDir = "/srv/media/sonarr";
+    port = 8989;
+    user = "nuexq";
+    group = "users";
+  };
+
+  services.bazarr = {
+    enable = true;
+    dataDir = "/srv/media/bazarr";
+    port = 6767;
+    user = "nuexq";
+    group = "users";
+  };
+
+  services.qbittorrent = {
+    enable = true;
+    # Config path for qBittorrent, will be created automatically if missing
+    config = qbittorrentConfig;
+    # Set download directory for completed downloads
+    downloadsDir = "/srv/media/downloads";
+    user = "nuexq";
+    group = "users";
+    # Optional: enable web-ui for remote control
+    webUiEnable = true;
+    webUiPort = 8080;
+  };
+
+  # Make sure these directories exist and permissions are correct:
+  systemd.tmpfiles.rules = [
+    "d /srv/media/radarr 0755 nuexq users -"
+    "d /srv/media/sonarr 0755 nuexq users -"
+    "d /srv/media/bazarr 0755 nuexq users -"
+    "d /srv/media/downloads 0755 nuexq users -"
+  ];
+
+  # Firewall ports open for these services
+  networking.firewall.allowedTCPPorts = with config.networking.firewall.allowedTCPPorts or []; [
+    7878  # Radarr
+    8989  # Sonarr
+    6767  # Bazarr
+    8080  # qBittorrent web UI
+  ];
+
+  # Optional: User should be in appropriate groups for permissions
+  users.users.nuexq.extraGroups = (config.users.users.nuexq.extraGroups or []) ++ [ "users" "networkmanager" "video" ];
+}
