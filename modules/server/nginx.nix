999241b8cafc45f9831e30dade33f783afe8f7d3
nuexq <hi.yassir.op@gmail.com>
2025-06-23 17:15:01 +0100

    nginx?

---
+++ modules/server/nginx.nix
@@ -0,0 +1,32 @@
+{ ... }:
+
+{
+  # ... (your existing configuration) ...
+
+  services.nginx = {
+    enable = true;
+    defaultEnable = true;
+
+    virtualHosts."rss.home.com" = {
+      listen = [{
+        addr = "192.168.1.8";
+        port = 80;
+      }];
+      serverName = "rss.home.com";
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8067";
+
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 ];
+}
