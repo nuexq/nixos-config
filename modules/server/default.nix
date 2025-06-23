e5ec3745e71eadba845a3d1f6719a7c22cb850d6
nuexq <hi.yassir.op@gmail.com>
2025-06-23 14:37:24 +0100

    miniflux

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./minecraft.nix # minecraft server
     ./jellyfin.nix  # jellyfin
+    ./miniflux.nix  # rss reader
   ];
 }
---
+++ modules/server/miniflux.nix
@@ -0,0 +1,20 @@
+{ ... }:
+
+{
+  services.miniflux = {
+    enable = true;
+
+    # Provide this file with:
+    adminCredentialsFile = /var/lib/miniflux/admin.env;
+
+    config = {
+      BASE_URL =
+        "http://192.168.1.8:8067";
+      CREATE_ADMIN = "1";
+      LISTEN_ADDR = "0.0.0.0:8067";
+
+      DISABLE_LOCAL_AUTH = "false";
+    };
+  };
+}
+
