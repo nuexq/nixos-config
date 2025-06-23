88a45ddc93c953f936f733c3918830f6fe28d2b4
nuexq <hi.yassir.op@gmail.com>
2025-06-23 18:55:11 +0100

    trying to fix miniflux

--- modules/server/miniflux.nix
+++ modules/server/miniflux.nix
@@ -3,12 +3,12 @@
 {
   services.miniflux = {
     enable = true;
-    adminCredentialsFile = "/etc/miniflux.env";
+    adminCredentialsFile = "/etc/miniflux_adming";
 
     config = {
       CREATE_ADMIN= "1";
       BASE_URL = "http://192.168.1.8:8067";
-      LISTEN_ADDR = "0.0.0.1:8067";
+      LISTEN_ADDR = "0.0.0.0:8067";
     };
   };
 
