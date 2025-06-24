e2ede63023c5b4c013ee0b116a0fa94b6bfa36e5
nuexq <hi.yassir.op@gmail.com>
2025-06-24 19:15:14 +0100

    ship of harkinian

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    gamemode                         # gamemoderun
-    mangohud                         # monitoring fps
-    umu-launcher                     # proton outside steam
-    nur.repos.${"0komo"}.sklauncher  # sklauncher
+    gamemode # gamemoderun
+    mangohud # monitoring fps
+    umu-launcher # proton outside steam
+    nur.repos.${"0komo"}.sklauncher # sklauncher
+    shipwright
   ];
 }
