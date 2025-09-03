0f24cb548edae339d38de7c8178da6857a7d853a
nuexq <nuexqq@gmail.com>
2025-09-03 19:14:42 +0100

    gamemode.enable = true

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
+  programs.gamemode.enable = true;
   home.packages = with pkgs; [
-    gamemode # gamemoderun
     mangohud # monitoring fps
     umu-launcher # proton outside steam
     nur.repos.${"0komo"}.sklauncher # sklauncher
