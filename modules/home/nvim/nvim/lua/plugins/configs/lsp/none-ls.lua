062deee2ed663ad4c110c15d8aa9252274d0a648
nuexq <nuexqq@gmail.com>
2025-11-18 16:57:43 +0100

    fix tex_fmt

--- modules/home/nvim/nvim/lua/plugins/configs/lsp/none-ls.lua
+++ modules/home/nvim/nvim/lua/plugins/configs/lsp/none-ls.lua
@@ -1,4 +1,20 @@
 local null_ls = require("null-ls")
+
+local tex_fmt = null_ls.register({
+	name = "tex_fmt",
+	method = null_ls.methods.FORMATTING,
+	filetypes = { "tex" },
+	generator = null_ls.formatter({
+		command = "tex-fmt",
+		args = { "--stdin", "--print" },
+		to_stdin = true,
+
+		on_output = function(params)
+			return params.output
+		end,
+	}),
+})
+
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
@@ -18,7 +34,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.prettier.with({ filetypes = { "astro", "markdown" } }),
 		null_ls.builtins.formatting.nixfmt,
-		null_ls.builtins.formatting.text_fmt,
+		tex_fmt,
 		null_ls.builtins.diagnostics.vale,
 	},
 })
