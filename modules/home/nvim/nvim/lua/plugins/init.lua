950ca50a4c42e68159f7d9787888ecc1d723fc9b
nuexq <nuexqq@gmail.com>
2025-11-18 12:55:40 +0100

    remove tailwind-tools.nvim plugin

--- modules/home/nvim/nvim/lua/plugins/init.lua
+++ modules/home/nvim/nvim/lua/plugins/init.lua
@@ -456,18 +456,6 @@ return {
 
 	-- Completion
 	{
-		"luckasRanarison/tailwind-tools.nvim",
-		name = "tailwind-tools",
-		build = ":UpdateRemotePlugins",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-telescope/telescope.nvim", -- optional
-			"neovim/nvim-lspconfig", -- optional
-		},
-		opts = {}, -- your configuration
-	},
-
-	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
