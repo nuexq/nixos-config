ac0bedfdcc73853344334772c5874d82591344a4
nuexq <nuexqq@gmail.com>
2025-12-03 16:59:00 +0100

    switching to tectonic anyway

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -2,8 +2,7 @@
   home.packages = with pkgs; [
     tree-sitter
     python313Packages.pylatexenc
-    texlive.combined.scheme-basic
-    texlivePackages.latexmk
+    tectonic
   ];
 
   programs.neovim = { enable = true; };
--- modules/home/nvim/nvim/lua/plugins/init.lua
+++ modules/home/nvim/nvim/lua/plugins/init.lua
@@ -455,7 +455,7 @@ return {
 		-- tag = "v2.15", -- uncomment to pin to a specific release
 		init = function()
 			vim.g.vimtex_view_method = "zathura"
-			vim.g.vimtex_compiler_method = "latexmk"
+			vim.g.vimtex_compiler_method = "tectonic"
 		end,
 	},
 
