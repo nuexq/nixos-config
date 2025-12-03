2ec41e9297a188e3291e39cd2f0d90c74c96a951
nuexq <nuexqq@gmail.com>
2025-12-03 16:09:24 +0100

    lua snippets

--- modules/home/nvim/nvim/lua/plugins/configs/cmp.lua
+++ modules/home/nvim/nvim/lua/plugins/configs/cmp.lua
@@ -1,4 +1,5 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -18,7 +19,7 @@ end
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
+			luasnip.lsp_expand(args.body)
 		end,
 	},
 	window = {
@@ -34,17 +35,32 @@ cmp.setup({
 		},
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<Tab>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.confirm({ select = true }) -- confirm completion if menu open
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump() -- jump in snippet
+			else
+				fallback() -- normal tab
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1) -- jump backward in snippet
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
 		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-k>"] = cmp.mapping.select_prev_item(),
 	}),
-	completion = { completeopt = "menu,menuone,noinsert" },
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
 	sources = cmp.config.sources({
-		{
-			name = "lazydev",
-			-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-			group_index = 0,
-		},
+		{ name = "lazydev", group_index = 0 },
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "path" },
