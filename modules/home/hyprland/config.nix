a41149180d790064f17dc5ca6fdd76c314b0fc9a
nuexq <nuexqq@gmail.com>
2026-01-04 19:23:22 +0100

    fix hyprland

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -41,8 +41,6 @@ in {
       };
 
       general = {
-        "$mainMod" = "SUPER";
-        "$hyper" = "SUPER CTRL ALT SHIFT";
         layout = "dwindle";
         gaps_in = 3;
         gaps_out = 8;
@@ -127,123 +125,123 @@ in {
 
       bind = [
         # show keybinds list
-        "$mainMod, F1, exec, vicinae 'vicinae://extensions/sovereign/hypr-keybinds/hyprland-keybinds'"
+        "SUPER, F1, exec, vicinae 'vicinae://extensions/sovereign/hypr-keybinds/hyprland-keybinds'"
 
         # keybindings
-        "$mainMod, H, exec, ghostty -e btop"
-        "$mainMod, Return, exec, ${terminal} --gtk-single-instance=true" # open terminal
-        "$mainMod, B, exec, ${browser}" # open browser
-        "$mainMod, Q, killactive," # kill active app
+        "SUPER, H, exec, ghostty -e btop"
+        "SUPER, Return, exec, ${terminal} --gtk-single-instance=true" # open terminal
+        "SUPER, B, exec, ${browser}" # open browser
+        "SUPER, Q, killactive," # kill active app
         "ALT, return, fullscreen" # go full screen mode
-        "$mainMod, W, exec, toggle-float"
+        "SUPER, W, exec, toggle-float"
         "ALT, Space, exec, vicinae toggle"
-        "$mainMod SHIFT, D, exec, vesktop --enable-features=UseOzonePlatform --ozone-platform=wayland"
-        "$mainMod, Escape, exec, hyprlock"
-        "$mainMod SHIFT, Escape, exec, vicinae 'vicinae://toggle?fallbackText=power%20management'"
-        "$mainMod, D, togglesplit," # toggle split
-        "$mainMod, E, exec, nemo"
+        "SUPER SHIFT, D, exec, vesktop --enable-features=UseOzonePlatform --ozone-platform=wayland"
+        "SUPER, Escape, exec, hyprlock"
+        "SUPER SHIFT, Escape, exec, vicinae 'vicinae://toggle?fallbackText=power%20management'"
+        "SUPER, D, togglesplit," # toggle split
+        "SUPER, E, exec, nemo"
         "ALT, E, exec, hyprctl dispatch exec '[float; size 1111 700] nemo'"
         "ALT, Escape, exec, toggle-waybar"
-        "$mainMod, C, exec, hyprpicker -a"
-        "$mainMod SHIFT, W, exec, vicinae vicinae://extensions/sovereign/awww-switcher/wpgrid"
-        "$mainMod, N, exec, swaync-client -t -sw"
-        "$mainMod, equal, exec, woomer"
+        "SUPER, C, exec, hyprpicker -a"
+        "SUPER SHIFT, W, exec, vicinae vicinae://extensions/sovereign/awww-switcher/wpgrid"
+        "SUPER, N, exec, swaync-client -t -sw"
+        "SUPER, equal, exec, woomer"
 
         # screenshot 
         ",Print, exec, screenshot --fullscreen" # Fullscreen screenshot instantly
-        "$mainMod, Print, exec, screenshot --nonfreeze-copy" # Non-freeze + select area → copy
-        "$hyper, S, exec, screenshot --freeze-copy" # Freeze + select area → copy
+        "SUPER, Print, exec, screenshot --nonfreeze-copy" # Non-freeze + select area → copy
+        "SUPER CTRL ALT SHIFT, S, exec, screenshot --freeze-copy" # Freeze + select area → copy
 
         # switch focus
-        "$mainMod, left,  movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up,    movefocus, u"
-        "$mainMod, down,  movefocus, d"
-        "$mainMod, h, movefocus, l"
-        "$mainMod, j, movefocus, d"
-        "$mainMod, k, movefocus, u"
-        "$mainMod, l, movefocus, r"
+        "SUPER, left,  movefocus, l"
+        "SUPER, right, movefocus, r"
+        "SUPER, up,    movefocus, u"
+        "SUPER, down,  movefocus, d"
+        "SUPER, h, movefocus, l"
+        "SUPER, j, movefocus, d"
+        "SUPER, k, movefocus, u"
+        "SUPER, l, movefocus, r"
 
-        "$mainMod, left,  alterzorder, top"
-        "$mainMod, right, alterzorder, top"
-        "$mainMod, up,    alterzorder, top"
-        "$mainMod, down,  alterzorder, top"
-        "$mainMod, h, alterzorder, top"
-        "$mainMod, j, alterzorder, top"
-        "$mainMod, k, alterzorder, top"
-        "$mainMod, l, alterzorder, top"
+        "SUPER, left,  alterzorder, top"
+        "SUPER, right, alterzorder, top"
+        "SUPER, up,    alterzorder, top"
+        "SUPER, down,  alterzorder, top"
+        "SUPER, h, alterzorder, top"
+        "SUPER, j, alterzorder, top"
+        "SUPER, k, alterzorder, top"
+        "SUPER, l, alterzorder, top"
 
         "CTRL ALT, up, exec, hyprctl dispatch focuswindow floating"
         "CTRL ALT, down, exec, hyprctl dispatch focuswindow tiled"
 
         # switch workspace
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
+        "SUPER, 1, workspace, 1"
+        "SUPER, 2, workspace, 2"
+        "SUPER, 3, workspace, 3"
+        "SUPER, 4, workspace, 4"
+        "SUPER, 5, workspace, 5"
+        "SUPER, 6, workspace, 6"
+        "SUPER, 7, workspace, 7"
+        "SUPER, 8, workspace, 8"
+        "SUPER, 9, workspace, 9"
+        "SUPER, 0, workspace, 10"
 
         # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-        "$mainMod CTRL, c, movetoworkspace, empty"
+        "SUPER SHIFT, 1, movetoworkspace, 1"
+        "SUPER SHIFT, 2, movetoworkspace, 2"
+        "SUPER SHIFT, 3, movetoworkspace, 3"
+        "SUPER SHIFT, 4, movetoworkspace, 4"
+        "SUPER SHIFT, 5, movetoworkspace, 5"
+        "SUPER SHIFT, 6, movetoworkspace, 6"
+        "SUPER SHIFT, 7, movetoworkspace, 7"
+        "SUPER SHIFT, 8, movetoworkspace, 8"
+        "SUPER SHIFT, 9, movetoworkspace, 9"
+        "SUPER SHIFT, 0, movetoworkspace, 10"
+        "SUPER CTRL, c, movetoworkspace, empty"
 
         # Move active window to a workspace silently
-        "$mainMod ALT, 1, movetoworkspacesilent, 1"
-        "$mainMod ALT, 2, movetoworkspacesilent, 2"
-        "$mainMod ALT, 3, movetoworkspacesilent, 3"
-        "$mainMod ALT, 4, movetoworkspacesilent, 4"
-        "$mainMod ALT, 5, movetoworkspacesilent, 5"
-        "$mainMod ALT, 6, movetoworkspacesilent, 6"
-        "$mainMod ALT, 7, movetoworkspacesilent, 7"
-        "$mainMod ALT, 8, movetoworkspacesilent, 8"
-        "$mainMod ALT, 9, movetoworkspacesilent, 9"
-        "$mainMod ALT, 0, movetoworkspacesilent, 10"
+        "SUPER ALT, 1, movetoworkspacesilent, 1"
+        "SUPER ALT, 2, movetoworkspacesilent, 2"
+        "SUPER ALT, 3, movetoworkspacesilent, 3"
+        "SUPER ALT, 4, movetoworkspacesilent, 4"
+        "SUPER ALT, 5, movetoworkspacesilent, 5"
+        "SUPER ALT, 6, movetoworkspacesilent, 6"
+        "SUPER ALT, 7, movetoworkspacesilent, 7"
+        "SUPER ALT, 8, movetoworkspacesilent, 8"
+        "SUPER ALT, 9, movetoworkspacesilent, 9"
+        "SUPER ALT, 0, movetoworkspacesilent, 10"
 
         # window control
-        "$mainMod SHIFT, left, movewindow, l"
-        "$mainMod SHIFT, right, movewindow, r"
-        "$mainMod SHIFT, up, movewindow, u"
-        "$mainMod SHIFT, down, movewindow, d"
-        "$mainMod SHIFT, h, movewindow, l"
-        "$mainMod SHIFT, j, movewindow, d"
-        "$mainMod SHIFT, k, movewindow, u"
-        "$mainMod SHIFT, l, movewindow, r"
+        "SUPER SHIFT, left, movewindow, l"
+        "SUPER SHIFT, right, movewindow, r"
+        "SUPER SHIFT, up, movewindow, u"
+        "SUPER SHIFT, down, movewindow, d"
+        "SUPER SHIFT, h, movewindow, l"
+        "SUPER SHIFT, j, movewindow, d"
+        "SUPER SHIFT, k, movewindow, u"
+        "SUPER SHIFT, l, movewindow, r"
 
-        "$mainMod CTRL, left, resizeactive, -40 0"
-        "$mainMod CTRL, right, resizeactive, 40 0"
-        "$mainMod CTRL, up, resizeactive, 0 -40"
-        "$mainMod CTRL, down, resizeactive, 0 40"
-        "$mainMod CTRL, h, resizeactive, -40 0"
-        "$mainMod CTRL, j, resizeactive, 0 40"
-        "$mainMod CTRL, k, resizeactive, 0 -40"
-        "$mainMod CTRL, l, resizeactive, 40 0"
+        "SUPER CTRL, left, resizeactive, -40 0"
+        "SUPER CTRL, right, resizeactive, 40 0"
+        "SUPER CTRL, up, resizeactive, 0 -40"
+        "SUPER CTRL, down, resizeactive, 0 40"
+        "SUPER CTRL, h, resizeactive, -40 0"
+        "SUPER CTRL, j, resizeactive, 0 40"
+        "SUPER CTRL, k, resizeactive, 0 -40"
+        "SUPER CTRL, l, resizeactive, 40 0"
 
-        "$mainMod ALT, left, moveactive,  -40 0"
-        "$mainMod ALT, right, moveactive, 40 0"
-        "$mainMod ALT, up, moveactive, 0 -40"
-        "$mainMod ALT, down, moveactive, 0 40"
-        "$mainMod ALT, h, moveactive,  -40 0"
-        "$mainMod ALT, j, moveactive, 0 40"
-        "$mainMod ALT, k, moveactive, 0 -40"
-        "$mainMod ALT, l, moveactive, 40 0"
+        "SUPER ALT, left, moveactive,  -40 0"
+        "SUPER ALT, right, moveactive, 40 0"
+        "SUPER ALT, up, moveactive, 0 -40"
+        "SUPER ALT, down, moveactive, 0 40"
+        "SUPER ALT, h, moveactive,  -40 0"
+        "SUPER ALT, j, moveactive, 0 40"
+        "SUPER ALT, k, moveactive, 0 -40"
+        "SUPER ALT, l, moveactive, 40 0"
 
         # Special workspaces (scratchpad)
-        "$mainMod ALT, S, movetoworkspacesilent, special"
-        "$mainMod, S, togglespecialworkspace,"
+        "SUPER ALT, S, movetoworkspacesilent, special"
+        "SUPER, S, togglespecialworkspace,"
 
         # media and volume controls
         # ",XF86AudioMute,exec, pamixer -t"
@@ -252,17 +250,17 @@ in {
         ",XF86AudioPrev,exec, playerctl previous"
         ",XF86AudioStop,exec, playerctl stop"
 
-        "$mainMod, mouse_down, workspace, e-1"
-        "$mainMod, mouse_up, workspace, e+1"
+        "SUPER, mouse_down, workspace, e-1"
+        "SUPER, mouse_up, workspace, e+1"
 
         # clipboard manager
-        "$mainMod, V, exec, vicinae vicinae://extensions/vicinae/clipboard/history"
+        "SUPER, V, exec, vicinae vicinae://extensions/vicinae/clipboard/history"
       ];
 
       # mouse binding
       bindm = [
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
+        "SUPER, mouse:272, movewindow"
+        "SUPER, mouse:273, resizewindow"
       ];
 
       # windowrule
