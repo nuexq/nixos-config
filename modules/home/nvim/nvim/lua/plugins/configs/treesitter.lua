66cfed61b38a7a8c8b69e1affee54eff05c1e1eb
nuexq <nuexqq@gmail.com>
2026-07-02 19:02:05 +0100

    fix treesitter

--- modules/home/nvim/nvim/lua/plugins/configs/treesitter.lua
+++ modules/home/nvim/nvim/lua/plugins/configs/treesitter.lua
@@ -1,11 +1,29 @@
-local config = require("nvim-treesitter.configs")
-config.setup({
-	auto_install = true,
-	highlight = {
-		enable = true,
-    disable = { "latex" },
-	},
-	indent = { enable = true },
+local ts = require("nvim-treesitter")
+
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function(args)
+		local lang = vim.treesitter.language.get_lang(args.match) or args.match
+		if lang == "" or lang == "latex" then return end
+		
+		local ok = pcall(vim.treesitter.get_parser, 0, lang)
+		if not ok then
+			pcall(ts.install, lang)
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function(args)
+		local lang = vim.treesitter.language.get_lang(args.match) or args.match
+
+		if lang == "latex" then
+			vim.bo.synmaxcol = 3000
+			return
+		end
+
+		pcall(vim.treesitter.start)
+		vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+	end,
 })
 
 vim.filetype.add({
