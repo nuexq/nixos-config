d52ca527f13616d1c5b5afe13b6cccdf38ec61e6
nuexq <nuexqq@gmail.com>
2026-07-03 18:39:28 +0100

    fix treesitter

--- modules/home/nvim/nvim/lua/plugins/configs/treesitter.lua
+++ modules/home/nvim/nvim/lua/plugins/configs/treesitter.lua
@@ -3,26 +3,22 @@ local ts = require("nvim-treesitter")
 vim.api.nvim_create_autocmd("FileType", {
 	callback = function(args)
 		local lang = vim.treesitter.language.get_lang(args.match) or args.match
-		if lang == "" or lang == "latex" then return end
-		
-		local ok = pcall(vim.treesitter.get_parser, 0, lang)
-		if not ok then
-			pcall(ts.install, lang)
-		end
-	end,
-})
 
-vim.api.nvim_create_autocmd("FileType", {
-	callback = function(args)
-		local lang = vim.treesitter.language.get_lang(args.match) or args.match
-
-		if lang == "latex" then
-			vim.bo.synmaxcol = 3000
+		if lang == "" or lang == "alpha" or lang == "latex" then
+			if lang == "latex" then
+				vim.bo.synmaxcol = 3000
+			end
 			return
 		end
 
-		pcall(vim.treesitter.start)
-		vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+		local has_parser = pcall(vim.treesitter.get_parser, 0, lang)
+
+		if not has_parser then
+			pcall(ts.install, { lang })
+		else
+			pcall(vim.treesitter.start)
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end
 	end,
 })
 
