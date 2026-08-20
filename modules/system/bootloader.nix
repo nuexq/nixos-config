31f7ec117177841a61d4dcd7e57495c1a86a1a4a
nuexq <nuexqq@gmail.com>
2026-08-20 18:01:25 +0100

    bootloader faster

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -1,11 +1,30 @@
 { ... }:
 {
   boot = {
+    plymouth.enable = true;
+
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
       systemd-boot.configurationLimit = 10;
-      timeout = 1;
+      timeout = 0;
     };
+
+    initrd = {
+      compressor = "zstd";
+      verbose = false;
+      systemd.enable = true;
+    };
+
+    consoleLogLevel = 0;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
   };
 }
