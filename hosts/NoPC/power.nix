063c2c5fed009a58fc82378cf381cae45477e5e8
nuexq <nuexqq@gmail.com>
2026-09-15 14:25:01 +0100

    thershold things

--- hosts/NoPC/power.nix
+++ hosts/NoPC/power.nix
@@ -53,7 +53,7 @@
 
   };
   systemd.tmpfiles.rules = [
-    "w /sys/class/power_supply/BAT0/charge_control_start_threshold - - - - 75"
+    "w /sys/class/power_supply/BAT0/charge_control_start_threshold - - - - 70"
     "w /sys/class/power_supply/BAT0/charge_control_end_threshold - - - - 80"
   ];
 
--- modules/system/security.nix
+++ modules/system/security.nix
@@ -1,14 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 {
   security = {
     rtkit.enable = true;
-    sudo.enable = true;
+    sudo = {
+      enable = true;
+      # for begin able to edit threshold without sudo (used in my quickshell)
+      extraRules = [
+        {
+          users = [ "nuexq" ];
+          commands = [
+            {
+              command = "/run/current-system/sw/bin/tee /sys/class/power_supply/BAT0/charge_control_start_threshold";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "/run/current-system/sw/bin/tee /sys/class/power_supply/BAT0/charge_control_end_threshold";
+              options = [ "NOPASSWD" ];
+            }
+          ];
+        }
+      ];
+    };
     pam.services.hyprlock = { };
   };
 
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-    # pinentryFlavor = "";
   };
 }
