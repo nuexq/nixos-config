02e61c4c19ef3765d58a01cd536663a800f32de6
nuexq <nuexqq@gmail.com>
2026-09-15 14:25:10 +0100

    some battery optimizations

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -25,6 +25,8 @@
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
+      "pcie_aspm=force"
+      "nvme_core.default_ps_max_latency_us=0"
     ];
   };
 }
