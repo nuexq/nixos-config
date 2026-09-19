1f7555c27b43722eec9620f0f4ee00198137fb17
nuexq <nuexqq@gmail.com>
2026-09-19 16:01:50 +0100

    deserted-font code

--- pkgs/deserted-font/default.nix
+++ pkgs/deserted-font/default.nix
@@ -2,26 +2,26 @@
 
 stdenv.mkDerivation {
   pname = "deserted-font";
-  version = "unstable-2022-12-26";
+  version = "unstable-2026-08-03";
 
   src = fetchFromGitHub {
-    owner = "wooosh";
-    repo = "deserted-font";
-    rev = "c356064b7b6973d34a2518d5ff364e58e19e7be1";
-    hash = "sha256-mTgIZetgbKiXu287pxbyfKP4+fiuOD8HP7tM/uK+Cug=";
+    owner = "falliah";
+    repo = "notepad-dots";
+    rev = "3e6bc134bf57f0887e0ddaf2e5f5f7aad3cc8b54";
+    hash = "sha256-UtWoC4LsJ/RD/pyDoV1hWPSZ+9/ScxQptcGwyhGXxqQ=";
   };
 
   dontBuild = true;
 
   installPhase = ''
     runHook preInstall
-    install -Dm644 shnm8x16a.bdf -t $out/share/fonts/misc
+    install -Dm644 desertedttf3.ttf -t $out/share/fonts/truetype
     runHook postInstall
   '';
 
   meta = {
-    description = "Deserted, a bitmap font by wooosh";
-    homepage = "https://github.com/wooosh/deserted-font";
+    description = "TTF conversion of the Deserted bitmap font, by falliah";
+    homepage = "https://github.com/falliah/notepad-dots";
     license = null;
   };
 }
