d4868ad14ec8d0cc5c1ce92c7c53d625c9925d2a
nuexq <nuexqq@gmail.com>
2026-09-19 16:01:23 +0100

    vimtex

--- modules/home/nvim/nvim/lua/plugins/init.lua
+++ modules/home/nvim/nvim/lua/plugins/init.lua
@@ -469,18 +469,18 @@ return {
 		init = function()
 			vim.g.vimtex_view_method = "zathura"
 			vim.g.vimtex_compiler_method = "latexmk"
+			vim.g.vimtex_view_forward_search_on_start = 0
 			vim.g.vimtex_quickfix_mode = 0
 			vim.g.vimtex_quickfix_open_on_warning = 0
 			vim.g.vimtex_quickfix_open_on_error = 0
 			vim.g.vimtex_compiler_latexmk = {
+				out_dir = "build",
 				options = {
 					"-shell-escape",
 					"-file-line-error",
 					"-synctex=1",
 					"-interaction=nonstopmode",
 					"-lualatex",
-					"-auxdir=../build",
-					"-outdir=.",
 				},
 			}
 		end,
